baf95adce7cd01591aadbec8f28c31f095abffaf
jinsung <201519011@yiu.ac.kr>
2023-10-31 20:29:03 +0900

    Init

---
+++ src/test/java/kr/co/futur/futurgateway/FuturGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.futur.futurgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
